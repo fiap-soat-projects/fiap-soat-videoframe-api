9fe1b33255f4ab2bb72c7fe261e844680b34249a
jefersondsgomes <jefersondsgomes@gmail.com>
2026-03-13 14:42:26 -0300

    fix: update webhook regex

--- src/Domain/ValueObjects/Webhook.cs
+++ src/Domain/ValueObjects/Webhook.cs
@@ -10,13 +10,13 @@ public readonly partial struct Webhook
     public static implicit operator Webhook(string? value) => new(value);
     public static implicit operator string(Webhook email) => email.Adress;
 
-    private static readonly Regex EmailRegex = new Regex(@"^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$", RegexOptions.Compiled);
+    private static readonly Regex WebhookRegex = new(@"^(https?:\/\/)?([\w\-]+(\.[\w\-]+)*)(:\d+)?(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$", RegexOptions.Compiled);
 
     public Webhook(string? address)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(address, "Webhook address cannot be null or white space");
 
-        if (EmailRegex.IsMatch(address) is false)
+        if (WebhookRegex.IsMatch(address) is false)
         {
             throw new InvalidWebhookException(address);
         }
